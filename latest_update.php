2db5a389526c8c60dcff010c548df48fc4c85482
Ian Bennett <webdev@ianbennett.net>
2013-05-14 20:08:07 +0100

    Gracefully handle latest update failures

--- latest_update.php
+++ latest_update.php
@@ -17,21 +17,17 @@ class SDALatestUpdate {
     $this->callback = $callback;
     
     // Try to read the cache (15 minutes)
-    $data = SDAStream::read_cache($callback, 15, 'jsonp');
-    if ($data) {
-      $this->results = $data;
-      return $this;
-    }
+    if ($this->read_cache(15)) return $this;
 
     // Else go get
-    $page = SDAStream::get_sock('http://speeddemosarchive.com/index.html');
+    $page = SDAStream::get_sock('http://speeddemosarchive.com/index.html') or $this->fail(true);
 
     // Get the update
-    if (!preg_match('/<p class="d"><b>\w+?, (\w+?) (\d{1,2}), (\d{4})<\/b> by (.+?)<\/p>/', $page, $latest)) die(); // error stuff here
+    preg_match('/<p class="d"><b>\w+?, (\w+?) (\d{1,2}), (\d{4})<\/b> by (.+?)<\/p>/', $page, $latest) or $this->fail();
 
     // Get the games
-    if (!preg_match('/<div class="b">.*?<\/div>/s', $page, $content)) die(); // error stuff here
-    if ( ($ct = preg_match_all('/<a href="(?:http\:\/\/(?:www\.)?speeddemosarchive\.com)?\/?(\w+?\.html)">(.+?)<\/a>/m', $content[0], $games)) === false ) die();
+    preg_match('/<div class="b">.*?<\/div>/s', $page, $content) or $this->fail(); 
+    if ( ($ct = preg_match_all('/<a href="(?:http\:\/\/(?:www\.)?speeddemosarchive\.com)?\/?(\w+?\.html)">(.+?)<\/a>/m', $content[0], $games)) === false ) $this->fail();
     $games2 = array();
     for ($i = 0; $i < $ct; $i++) $games2[] = array('path' => $games[1][$i], 'title' => $games[2][$i]);
 
@@ -47,6 +43,32 @@ class SDALatestUpdate {
   
   public function output($format = 'jsonp') {
     return SDAStream::serialize($this->results, $format, $this->callback);
+  }
+  
+  private function read_cache($timeout = null) {
+    $data = SDAStream::read_cache($this->callback, $timeout, 'jsonp');
+    if ($data) {
+      $this->results = $data;
+      return true;
+    }
+    return false;
+  }
+  
+  private function fail($http = false) {
+    // First attempt to read the cache again, with no limit on date
+    $success = $this->read_cache();
+    // Reset the timeout if the cache was read successfully, or if the issue was caused by HTTP failure
+    if ($success || $http) SDAStream::write_cache($this->callback, null, 'jsonp', true);
+    // Write a skeleton cache if the issue was caused by the markup on SDA
+    else {
+      $this->results = array(
+        'date'    => 0,
+        'author'  => 'No one',
+        'games'   => array(),
+      );
+      SDAStream::write_cache($this->callback, $this->results);
+    }
+    return $this;
   }
   
 }
